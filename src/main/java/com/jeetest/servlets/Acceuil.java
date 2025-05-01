6252ac2949f495745c02a918628c57fa709c2d9e
malcovys <malcovys22.aps@gmail.com>
2025-05-01 20:35:16 +0300

    cookie

--- src/main/java/com/jeetest/servlets/Acceuil.java
+++ src/main/java/com/jeetest/servlets/Acceuil.java
@@ -3,6 +3,7 @@ package com.jeetest.servlets;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,18 +22,34 @@ public class Acceuil extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// LIRE COOKIE
+		Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("prenom")) {
+                    request.setAttribute("prenom", cookie.getValue());
+                }
+            }
+        }
+		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/acceuil.jsp").forward(request, response);
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String nom = request.getParameter("nom");
+		// String nom = request.getParameter("nom");
 		String prenom = request.getParameter("prenom");
+
+		// COOKIE
+		Cookie cookie = new Cookie("prenom", prenom);
+		cookie.setMaxAge(60 * 60 * 24 * 30); // supprimer dans 30 jour
+		response.addCookie(cookie);
 		
-		HttpSession session = request.getSession();
 		
-		session.setAttribute("nom", nom);
-		session.setAttribute("prenom", prenom);
+		// CREE UNE SESSION		
+//		HttpSession session = request.getSession();
+//		session.setAttribute("nom", nom);
+//		session.setAttribute("prenom", prenom);
 		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/acceuil.jsp").forward(request, response);
 	}
--- src/main/webapp/WEB-INF/acceuil.jsp
+++ src/main/webapp/WEB-INF/acceuil.jsp
@@ -7,9 +7,13 @@
 <title>Acceuil</title>
 </head>
 <body>
-	<c:if test="${ !empty sessionScope.nom && !empty sessionScope.prenom }">
-		<p>Vous êtes ${ sessionScope.nom } ${ sessionScope.prenom } !</p>
-	</c:if>
+	<%-- LIRE UNE SESSION
+		<c:if test="${ !empty sessionScope.nom && !empty sessionScope.prenom }">
+			<p>Vous êtes ${ sessionScope.nom } ${ sessionScope.prenom } !</p>
+		</c:if> 
+	--%>
+	
+	<c:out value="${ prenom }" />
 
 	<form method="post" action="acceuil">
 		<p>
